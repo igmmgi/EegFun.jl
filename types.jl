03ae34ba764b275c03e6fc2815484a6bc48968a1
igmmgi <ian.mackenzie@uni-tuebingen.de>
2024-11-05 14:06:31 +0100

    updated vlines for triggers

--- types.jl
+++ types.jl
@@ -35,19 +35,3 @@ function Base.show(io::IO, dat::ErpData)
   println(io, "Sample Rate: ", dat.sample_rate)
 end
 
-function create_eeg_dataframe(data::BioSemiBDF.BioSemiData)
-  return hcat(DataFrame(time=data.time, events=data.triggers.raw),
-    DataFrame(data.data, Symbol.(data.header.channel_labels[1:end-1])))
-end
-
-function create_eeg_dataframe(dat::BioSemiBDF.BioSemiData, layout_file_name::String)
-  return ContinuousData(create_eeg_dataframe(dat), DataFrame(CSV.File(layout_file_name)), dat.header.sample_rate[1])
-end
-
-function create_eeg_dataframe(dat::BioSemiBDF.BioSemiData, layout::DataFrame)
-  return ContinuousData(create_eeg_dataframe(dat), layout, dat.header.sample_rate[1])
-end
-
-
-
-
