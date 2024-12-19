8309c51dd4310ee70c88a75603aca5ad24f6cacb
igmmgi <ian.mackenzie@uni-tuebingen.de>
2024-12-19 14:49:39 +0100

    updated plotting

--- topo.jl
+++ topo.jl
@@ -10,3 +10,20 @@ function circle_mask!(dat, grid_scale)
     end
   end
 end
+
+
+function data_interpolation_topo(dat, points; grid_scale=300)
+
+  radius = 88 # mm
+  x = y = range(-radius, radius, length=grid_scale)
+  X, Y = repeat(x', grid_scale)[:], repeat(y', grid_scale)[:]
+  grid = [X Y]'
+  dat = interpolate(Multiquadratic(), points, dat)
+  dat = ScatteredInterpolation.evaluate(dat, grid)
+  dat = reshape(dat, grid_scale, grid_scale)
+  circle_mask!(dat, grid_scale)
+  return dat
+end
+
+
+
