4cd0e40623a507d9249fbd713d989e62acc6b515
igmmgi <ian.mackenzie@uni-tuebingen.de>
2025-04-08 16:19:02 +0200

    plot updates

--- sandbox.jl
+++ sandbox.jl
@@ -44,7 +44,7 @@ diff_channel!(dat, :F9, :F10, :hEOG);
 detect_eog_onsets!(dat, 50, :vEOG, :is_vEOG)
 detect_eog_onsets!(dat, 30, :hEOG, :is_hEOG)
 is_extreme_value!(dat, dat.layout.label, 50);
- plot_databrowser(dat);
+plot_databrowser(dat);
 # extract epochs
 
 epochs = []
