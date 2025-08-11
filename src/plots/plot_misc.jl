de378689ea7c3a1640849637ae0d42a1f32f4d8d
igmmgi <ian.mackenzie@uni-tuebingen.de>
2025-08-11 16:21:29 +0200

    latest

--- src/plots/plot_misc.jl
+++ src/plots/plot_misc.jl
@@ -5,7 +5,9 @@ end
 
 function get_makie_backend()
     backend_str = string(Makie.current_backend())
-    if occursin("GLMakie", backend_str)
+    if occursin("WGLMakie", backend_str)
+        return :WGLMakie
+    elseif occursin("GLMakie", backend_str)
         return :GLMakie
     elseif occursin("CairoMakie", backend_str)
         return :CairoMakie
