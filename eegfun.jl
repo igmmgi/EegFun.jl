de42e060f87df4130ae1f996e3204cc5a82c4643
igmmgi <ian.mackenzie@uni-tuebingen.de>
2025-09-15 15:41:53 +0200

    plot_erp_image refactor

--- eegfun.jl
+++ eegfun.jl
@@ -105,7 +105,7 @@ eegfun.plot_erp_image(epochs[1], channel_selection = eegfun.channels([:Fp1]), pl
 
 # TODO: x limit consistency between erp image and erp waveform
 eegfun.plot_erp_image(epochs[1], layout = :single) 
-eegfun.plot_erp_image(epochs[1], channel_selection = eegfun.channels([:Fp1, :Fp2]), layout = :single) 
+fig, axes = eegfun.plot_erp_image(epochs[1], channel_selection = eegfun.channels([:Fp1, :Fp2]), layout = :single, boxcar_average = 20, colorrange = (-20, 20)) 
 # TODO: no electrode labels in title
 eegfun.plot_erp_image(epochs[1], layout = :topo) 
 eegfun.plot_erp_image(epochs[1], layout = :grid) 
