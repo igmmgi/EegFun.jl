0b5054cef1de1a3c734004560387f95751447b69
igmmgi <ian.mackenzie@uni-tuebingen.de>
2025-10-08 08:31:45 +0200

    fixed plot_ica_topoplot defaults

--- test/manual/ica.jl
+++ test/manual/ica.jl
@@ -1,5 +1,6 @@
 using eegfun
 using GLMakie
+using BenchmarkTools
 
 # Get some basic data with initial preprocessing steps (high-pass filter, epoch)
 data_file = joinpath(@__DIR__, "..", "..", "..",  "Flank_C_3.bdf")
@@ -10,28 +11,38 @@ dat = eegfun.create_eeg_dataframe(dat, layout_file);
 eegfun.rereference!(dat, :avg)
 eegfun.filter_data!(dat, "hp", 1)
 eegfun.is_extreme_value!(dat, 100);
-ica_result = eegfun.run_ica(dat; sample_selection = eegfun.samples_not(:is_extreme_value_100))
+@btime ica_result = eegfun.run_ica(dat; sample_selection = eegfun.samples_not(:is_extreme_value_100))
 
-
-
-# some ICA plots
+# plot_ica_topoplot
 eegfun.plot_ica_topoplot(ica_result)
 eegfun.plot_ica_topoplot(ica_result, component_selection = eegfun.components(1:4), plot_labels = false);
+eegfun.plot_ica_topoplot(ica_result, component_selection = eegfun.components(1:10), plot_colorbar = true);
+eegfun.plot_ica_topoplot(ica_result, component_selection = eegfun.components(1:10), plot_colorbar = true, colorbar_plot_numbers = [1]);
+eegfun.plot_ica_topoplot(ica_result, component_selection = eegfun.components(1:10), use_global_scale = true);
+eegfun.plot_ica_topoplot(ica_result; component_selection = eegfun.components(1:10), method = :spherical_spline)
+eegfun.plot_ica_topoplot(ica_result; component_selection = eegfun.components(1:10), method = :multiquadratic)
+
+eegfun.plot_ica_topoplot(
+    ica_result,
+    component_selection = eegfun.components([1, 3, 5]);
+    use_global_scale = true,
+    plot_colorbar = true,
+    colorbar_plot_numbers = [2],
+)
+
+eegfun.plot_ica_topoplot(
+    ica_result,
+    component_selection = eegfun.components([1, 3, 5, 7, 9]);
+    dims = (2, 3),
+    use_global_scale = true,
+    plot_colorbar = true,
+    colorbar_plot_numbers = [5],
+)
 
 
+# plot_ica_component_activation
+eegfun.plot_ica_component_activation(dat, ica_result)
 
-
-
-
-
-
-
-
-
-eegfun.plot_ica_topoplot(ica_result, component_selection = eegfun.components(1:15), plot_labels = false);
-eegfun.plot_ica_topoplot(ica_result, component_selection = eegfun.components(1:15), plot_colorbar = true);
-eegfun.plot_ica_topoplot(ica_result, component_selection = eegfun.components(1:15), plot_colorbar = true, colorbar_plot_numbers = [1]);
-eegfun.plot_ica_topoplot(ica_result, component_selection = eegfun.components(1:15), use_global_scale = true);
 
 
 GLMakie.closeall()
