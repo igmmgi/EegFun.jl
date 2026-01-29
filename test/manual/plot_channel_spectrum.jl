39a111dddb95f5f10c9bcc967b2d1b951f01b141
igmmgi <ian.mackenzie@uni-tuebingen.de>
2026-01-29 15:09:14 +0100

    updated manual test examples

---
+++ test/manual/plot_channel_spectrum.jl
@@ -0,0 +1,16 @@
+using EegFun
+
+# read raw data
+dat = EegFun.read_raw_data("./data/raw_files/example1.bdf");
+
+# read and preprate layout file
+layout_file = EegFun.read_layout("./data/layouts/biosemi/biosemi72.csv");
+EegFun.polar_to_cartesian_xy!(layout_file)
+
+# create EegFun data structure (EegFun.ContinuousData)
+dat = EegFun.create_eeg_dataframe(dat, layout_file);
+
+# Plots
+EegFun.plot_channel_spectrum(dat)
+EegFun.plot_channel_spectrum(dat, channel_selection = EegFun.channels([:Fp1]), title = "Fp1 Power Spectrum")
+EegFun.plot_channel_spectrum(dat, channel_selection = EegFun.channels([:Fp1, :Fp2, :F3, :F4]), title = "Frontal Channels Power Spectrum")
