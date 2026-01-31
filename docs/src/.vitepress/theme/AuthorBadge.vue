6ea51e7d5b42e56d9ac85170478973415a06921c
igmmgi <ian.mackenzie@uni-tuebingen.de>
2026-01-31 17:22:11 +0100

    doc tests

--- docs/src/.vitepress/theme/AuthorBadge.vue
+++ docs/src/.vitepress/theme/AuthorBadge.vue
@@ -1,16 +1,1 @@
-<template>
-  <span class="author-badge">
-    <slot></slot>
-  </span>
-</template>
-
-<script setup lang="ts">
-</script>
-
-<style scoped>
-.author-badge {
-  display: inline-block;
-  padding: 0.2em 0.5em;
-  font-size: 0.9em;
-}
-</style>
+<template><span><slot /></span></template>
