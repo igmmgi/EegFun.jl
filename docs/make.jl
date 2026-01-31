29da4732d4103ec0b50144db21cc29af8452aaab
igmmgi <ian.mackenzie@uni-tuebingen.de>
2026-01-31 18:52:22 +0100

    test docs

--- docs/make.jl
+++ docs/make.jl
@@ -2,31 +2,31 @@ using Documenter
 using DocumenterVitepress
 
 # Add the parent directory to the load path so we can load the local package
-push!(LOAD_PATH, dirname(@__DIR__))
+# push!(LOAD_PATH, dirname(@__DIR__))
 
 using EegFun
 
-# Define page structure following Diátaxis framework
-pages = [
-    "Home" => "index.md",
-    "Tutorials" => [
-        "Getting Started" => "tutorials/getting-started.md",
-        "Basic Preprocessing" => "tutorials/basic-preprocessing.md",
-        "ERP Analysis" => "tutorials/erp-analysis.md",
-        "ICA Workflow" => "tutorials/ica-workflow.md",
-    ],
-    "How-To Guides" => [
-        "Filter Data" => "how-to/filter-data.md",
-        "Create Epochs" => "how-to/create-epochs.md",
-        "Topographic Plots" => "how-to/topographic-plots.md",
-    ],
-    "Explanations" => ["Data Structures" => "explanations/data-structures.md", "Statistical Methods" => "explanations/statistics.md"],
-    "Reference" => [
-        "Overview" => "reference/index.md",
-        "Types" => "reference/types.md",
-        # TODO: Add more reference pages as needed
-    ],
-]
+# # Define page structure following Diátaxis framework
+# pages = [
+#     "Home" => "index.md",
+#     "Tutorials" => [
+#         "Getting Started" => "tutorials/getting-started.md",
+#         "Basic Preprocessing" => "tutorials/basic-preprocessing.md",
+#         "ERP Analysis" => "tutorials/erp-analysis.md",
+#         "ICA Workflow" => "tutorials/ica-workflow.md",
+#     ],
+#     "How-To Guides" => [
+#         "Filter Data" => "how-to/filter-data.md",
+#         "Create Epochs" => "how-to/create-epochs.md",
+#         "Topographic Plots" => "how-to/topographic-plots.md",
+#     ],
+#     "Explanations" => ["Data Structures" => "explanations/data-structures.md", "Statistical Methods" => "explanations/statistics.md"],
+#     "Reference" => [
+#         "Overview" => "reference/index.md",
+#         "Types" => "reference/types.md",
+#         # TODO: Add more reference pages as needed
+#     ],
+# ]
 
 # Create version directory structure for DocumenterVitepress (workaround for siteinfo.js bug)
 # mkpath(joinpath(@__DIR__, "build", "1"))
@@ -59,9 +59,27 @@ makedocs(;
         deploy_url = "https://igmmgi.github.io/EegFun.jl",
     ),
     warnonly = [:linkcheck, :cross_references, :missing_docs],
+    pages = [
+        "Home" => "index.md",
+        "Tutorials" => [
+            "Getting Started" => "tutorials/getting-started.md",
+            "Basic Preprocessing" => "tutorials/basic-preprocessing.md",
+            "ERP Analysis" => "tutorials/erp-analysis.md",
+            "ICA Workflow" => "tutorials/ica-workflow.md",
+        ],
+        "How-To Guides" => [
+            "Filter Data" => "how-to/filter-data.md",
+            "Create Epochs" => "how-to/create-epochs.md",
+            "Topographic Plots" => "how-to/topographic-plots.md",
+        ],
+        "Explanations" => ["Data Structures" => "explanations/data-structures.md", "Statistical Methods" => "explanations/statistics.md"],
+        "Reference" => [
+            "Overview" => "reference/index.md",
+            "Types" => "reference/types.md",
+            # TODO: Add more reference pages as needed
+        ],
+    ],
 )
-
-
 
 
 DocumenterVitepress.deploydocs(
