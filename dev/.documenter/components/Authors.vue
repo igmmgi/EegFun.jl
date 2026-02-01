cdb7957d7249a6044c4660cd2dde2d8ec24f5ffb
Documenter.jl <documenter@juliadocs.github.io>
2026-02-01 12:39:59 +0000

    build based on f1453b4

--- dev/.documenter/components/Authors.vue
+++ dev/.documenter/components/Authors.vue
@@ -1,14 +1,28 @@
-<script setup>
-</script>
-
 <template>
-  <div class="authors">
-    <!-- Authors component content -->
-  </div>
-</template>
-
-<style scoped>
-.authors {
-  margin-top: 1rem;
-}
-</style>
+    <div class="authors-container">
+      <AuthorBadge
+        v-for="author in pageAuthors"
+        :key="author.name"
+        :author="author.name"
+        :avatar="author.avatar"
+        :platform="author.platform"
+        :link="author.link"
+      />
+    </div>
+  </template>
+  
+  <script setup>
+  import { useData } from 'vitepress'
+  
+  const { frontmatter } = useData()
+  const pageAuthors = frontmatter.value.authors || []
+  </script>
+  
+  <style scoped>
+  .authors-container {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 0.5rem;
+    margin: 1rem 0;
+  }
+  </style>
