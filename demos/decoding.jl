365618560026e606c9d8f31baf436557500ba660
igmmgi <ian.mackenzie@uni-tuebingen.de>
2026-02-09 21:29:54 +0100

    demos

--- demos/decoding.jl
+++ demos/decoding.jl
@@ -94,7 +94,7 @@ participant_epochs = EegFun.prepare_decoding(
     participant_selection = EegFun.participants(),
     condition_selection = EegFun.conditions([1, 2]),  # Compare conditions 1 and 2
     channel_selection = EegFun.channels(),            # All channels
-    sample_selection = EegFun.samples((-0.2, 1.5)),   # Time window
+    interval_selection = EegFun.times((-0.2, 1.5)),   # Time window
 )
 
 # Decode all participants (batch method)
