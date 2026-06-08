4e3e7f6cb39d87fa51a6d4a5861f85c6a325d54a
igmmgi <ian.mackenzie@uni-tuebingen.de>
2026-06-08 15:14:54 +0200

    feat: expand precompilation coverage

--- src/precompile.jl
+++ src/precompile.jl
@@ -10,10 +10,44 @@ PrecompileTools.@compile_workload begin
     if isfile(data_file) && isfile(layout_file_path)
         println("Precompiling EegFun...")
 
-        # 1. Reading *.bdf files and opening databrowser
+        # 1. Reading raw data files (all formats) and opening databrowser
+        # BDF
         dat = EegFun.read_raw_data(data_file)
         layout_file = EegFun.read_layout(layout_file_path)
         dat = EegFun.create_eegfun_data(dat, layout_file)
+
+        # BrainVision
+        bv_file = joinpath(pkg_dir, "resources", "data", "brainvision", "example1.vhdr")
+        if isfile(bv_file)
+            bv_raw = EegFun.read_raw_data(bv_file)
+            bv_dat = EegFun.create_eegfun_data(bv_raw)
+        end
+
+        # EDF
+        edf_file = joinpath(pkg_dir, "resources", "data", "edf", "test.edf")
+        if isfile(edf_file)
+            edf_raw = EegFun.read_raw_data(edf_file)
+            edf_dat = EegFun.create_eegfun_data(edf_raw)
+        end
+
+        # FIF (raw and epochs)
+        fif_raw_file = joinpath(pkg_dir, "resources", "data", "fif", "test_raw.fif")
+        if isfile(fif_raw_file)
+            fif_raw = EegFun.read_raw_data(fif_raw_file)
+            fif_dat = EegFun.create_eegfun_data(fif_raw)
+        end
+        fif_epo_file = joinpath(pkg_dir, "resources", "data", "fif", "test_epochs.fif")
+        if isfile(fif_epo_file)
+            fif_epo = EegFun.read_raw_data(fif_epo_file)
+            fif_epo_dat = EegFun.create_eegfun_data(fif_epo)
+        end
+
+        # XDF
+        xdf_file = joinpath(pkg_dir, "resources", "data", "xdf", "test1.xdf")
+        if isfile(xdf_file)
+            xdf_raw = EegFun.read_raw_data(xdf_file)
+            xdf_dat = EegFun.create_eegfun_data(xdf_raw)
+        end
 
         # 2. Common preprocessing pipeline
         EegFun.rereference!(dat, :avg)
@@ -29,9 +63,55 @@ PrecompileTools.@compile_workload begin
         erp = EegFun.average_epochs(epochs)
 
         # 5. Key plot types (headless — display_plot = false prevents window creation)
-        EegFun.plot_databrowser(dat; display_plot = false)
-        EegFun.plot_epochs(epochs[1]; display_plot = false)
-        EegFun.plot_erp(erp; display_plot = false)
+        res_db = EegFun.plot_databrowser(dat; display_plot = false)
+        plot_kwargs = EegFun._merge_plot_kwargs(EegFun.PLOT_DATABROWSER_KWARGS, NamedTuple())
+        state_con = EegFun._create_browser_state(dat, dat.layout.data.label, res_db.ax, nothing, plot_kwargs)
+        EegFun._xforward!(res_db.ax, state_con)
+        EegFun._xback!(res_db.ax, state_con)
+        EegFun._apply_hp_filter!(state_con)
+        EegFun._apply_lp_filter!(state_con)
+        EegFun._apply_filters!(state_con)
+        EegFun._butterfly_plot!(res_db.ax, state_con)
+        EegFun._subset_selected_data(state_con)
+        EegFun._clear_all_selected_regions!(res_db.ax, state_con)
+        EegFun._get_selected_regions_info(state_con)
+        state_con.channels.visible[1] = false
+        EegFun._draw(res_db.ax, state_con)
+
+        res_db_epoch = EegFun.plot_databrowser(epochs[1]; display_plot = false)
+        state_epoch = EegFun._create_browser_state(epochs[1], epochs[1].layout.data.label, res_db_epoch.ax, nothing, plot_kwargs)
+        EegFun._step_epoch_forward(res_db_epoch.ax, state_epoch)
+        EegFun._step_epoch_backward(res_db_epoch.ax, state_epoch)
+        EegFun._repair_selected_channels!(state_epoch, [epochs[1].layout.data.label[1]], :neighbor_interpolation, res_db_epoch.ax)
+        EegFun._repair_selected_channels!(state_epoch, [epochs[1].layout.data.label[1]], :spherical_spline, res_db_epoch.ax)
+        EegFun._undo_last_repair!(state_epoch, res_db_epoch.ax)
+
+        res_epochs = EegFun.plot_epochs(epochs[1]; display_plot = false)
+        EegFun._handle_shared_navigation!(res_epochs.axes, :up, 0.2)
+        EegFun._handle_shared_navigation!(res_epochs.axes, :down, 0.2)
+        EegFun._handle_shared_navigation!(res_epochs.axes, :left, 0.2)
+        EegFun._handle_shared_navigation!(res_epochs.axes, :right, 0.2)
+        
+        selection_state_epochs = EegFun.SharedSelectionState(res_epochs.axes; selection_color = :blue, selection_alpha = 0.3)
+        EegFun._start_shared_selection!(first(res_epochs.axes), selection_state_epochs, 0.1)
+        EegFun._update_shared_selection!(first(res_epochs.axes), selection_state_epochs, 0.1, 0.5)
+        EegFun._finish_shared_selection!(first(res_epochs.axes), selection_state_epochs, 0.5)
+        EegFun._get_epochs_selection_bounds(selection_state_epochs, [epochs[1]])
+        EegFun._clear_shared_selection!(selection_state_epochs)
+
+        res_erp = EegFun.plot_erp(erp; display_plot = false)
+        EegFun._handle_shared_navigation!(res_erp.axes, :up, 0.2)
+        EegFun._handle_shared_navigation!(res_erp.axes, :left, 0.2)
+        
+        selection_state_erp = EegFun.SharedSelectionState(res_erp.axes; selection_color = :blue, selection_alpha = 0.3)
+        EegFun._start_shared_selection!(first(res_erp.axes), selection_state_erp, 0.1)
+        EegFun._update_shared_selection!(first(res_erp.axes), selection_state_erp, 0.1, 0.5)
+        EegFun._finish_shared_selection!(first(res_erp.axes), selection_state_erp, 0.5)
+        EegFun._get_erp_selection_bounds(selection_state_erp, erp)
+        EegFun._filter_visible_conditions(erp, Ref(nothing))
+        EegFun._average_conditions(erp)
+        EegFun._clear_shared_selection!(selection_state_erp)
+
         EegFun.plot_topography(erp; display_plot = false)
 
         # 6. Predicate generators 
