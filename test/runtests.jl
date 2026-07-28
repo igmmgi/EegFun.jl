af683e174e92d2f5b34985d96d9d1851b24e36f7
igmmgi <ian.mackenzie@uni-tuebingen.de>
2026-07-28 14:49:47 +0200

    feat: configure GPU weak dependencies and update Aqua test compatibility settings

--- test/runtests.jl
+++ test/runtests.jl
@@ -10,7 +10,6 @@ println("="^40)
         EegFun;
         ambiguities = false,
         persistent_tasks = false,
-        stale_deps = (ignore = [:AMDGPU],),
         deps_compat = (check_extras = false, ignore = [:Dates, :LinearAlgebra, :Logging, :Printf, :Random, :SparseArrays, :TOML, :Test]),
     )
 end
